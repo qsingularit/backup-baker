c6abd6cea3ecc6f6425844e5e2aed622e9a9d385
QSNIT <info@qsingularit.com>
2018-04-25 00:47:04 -0400

    Adding S3 offload features

--- backup.sh
+++ backup.sh
@@ -1,6 +1,55 @@
 #! /bin/sh
 
 set -e
-function LocalBackup () {
-find $SPATH -type d -maxdepth 1 -mindepth 1 -exec tar cf $DPATH/{}-$(date +%F-%H-%M-%S-%Z).tar.gz {}  \;
+DUMPDATE=$(date +%F-%H-%M-%S-%Z)
+
+if ["${S3_ACCESS_KEY_ID}" == "null"] || ["${S3_SECRET_ACCESS_KEY}" == "null"] || ["${S3_BUCKET}" == "null"]; then
+
+    echo "No AWS S3 credentials or bucket is supplied. Making a local backup only."
+    Local_Backup
+fi
+
+function Local_Backup () {
+    if ["$(ls -A $SPATH)"]; then
+        find $SPATH -type d -maxdepth 1 -mindepth 1 -exec tar cf $DPATH/{}-${DUMPDATE}.tar.gz {}  \;
+    else
+        echo "Found an empty directory. Nothing to do."
+    fi
 }
+
+#S3 offload function to call on each backup file
+function Copy_To_S3 () {
+  SRC_FILE=$1
+  DEST_FILE=$2
+
+  if [ "${S3_ENDPOINT}" == "null" ]; then
+    AWS_ARGS=""
+  else
+    AWS_ARGS="--endpoint-url ${S3_ENDPOINT}"
+  fi
+
+  echo "Uploading ${DEST_FILE} on S3..."
+
+  cat $SRC_FILE | aws $AWS_ARGS s3 cp - s3://$S3_BUCKET/$S3_PREFIX/$DEST_FILE
+
+  if [ $? != 0 ]; then
+    >&2 echo "Error uploading ${DEST_FILE} on S3"
+  fi
+
+  rm $SRC_FILE
+}
+
+#Search latest modified file for past 24 hours or less and copying it to S3
+
+FILELIST=$(find . 2>/dev/null -type f -maxdepth 1 -mindepth 1 -mtime -1 -printf "%f\n" | sort)
+
+for i in $FILELIST; do
+
+    echo "Creating individual full backup of ${i} from ${$DPATH} to S3 ${S3_BUCKET/$S3_PREFIX} \n"
+    Copy_To_S3 ${i} ${i}
+done
+
+    if [ $? == 0 ]; then
+        echo "Backup to S3 complete!"
+    fi
+
