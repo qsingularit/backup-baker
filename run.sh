a9088434e69bced0ee0bff9d540af025163bd69b
QSNIT <info@qsingularit.com>
2018-08-16 20:26:03 -0400

    add error handling

--- run.sh
+++ run.sh
@@ -1,13 +1,16 @@
-#! /bin/sh
+#!/bin/sh
 
 set -e
 
 if [ "${SCHEDULE}" = "null" ]; then
     echo "No schedule, running once."
   sh /usr/share/backup.sh
+
 else
   exec go-cron "$SCHEDULE" /bin/sh /usr/share/backup.sh
 
-        exec go-cron "$SCHEDULE" /bin/sh /usr/share/rotate.sh
+  exec go-cron "$SCHEDULE" /bin/sh /usr/share/rotate.sh
 
 fi
+
+exit 0
