367b667366c93efbe63fba892a4d1d4135c509ef
Eugene Karmazin <info@qsingularit.com>
2019-03-16 21:24:48 -0400

    add log

--- run.sh
+++ run.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
 
+touch /var/log/bb.log;
+
 set -e
 
-if [ "${SCHEDULE}" = "null" ]; then
-    echo "No schedule, running once."
-  sh /usr/share/backup.sh
+    if [[ "${SCHEDULE}" = "null" ]]; then
 
-else
-  exec go-cron "$SCHEDULE" /bin/sh /usr/share/backup.sh
+        echo "No schedule, running once" >> /var/log/bb.log
+        sh /usr/share/backup.sh
 
-  exec go-cron "$SCHEDULE" /bin/sh /usr/share/rotate.sh
+    else
 
-fi
+      exec go-cron "$SCHEDULE" /bin/sh /usr/share/backup.sh;
+        echo "Set schedule for backup at ${SCHEDULE}" >> /var/log/bb.log;
+      exec go-cron "$SCHEDULE" /bin/sh /usr/share/rotate.sh
+        echo "Set schedule for cleanup at ${SCHEDULE}" >> /var/log/bb.log;
+    fi
 
 exit 0
